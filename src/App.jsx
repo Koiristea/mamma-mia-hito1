048c69037707488f91714dc81496ccfcb63e491f
Koiristea <koiristeart@gmail.com>
2025-02-02 20:19:22 -0300

    hito 1
    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './assets/components/Footer'
+import Home from './assets/components/Home'
+import Navbar from './assets/components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    </>
+  )
+}
+
+export default App
